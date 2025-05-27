dddd9eefb5d944ea51a4a168e456e63681dc2a9d
vb3 <vbonamassa@proton.me>
2025-05-27 10:31:57 -0400

    new add

--- src/main/java/edu/farmingdale/csc311_gui_basics/HelloApplication.java
+++ src/main/java/edu/farmingdale/csc311_gui_basics/HelloApplication.java
@@ -12,7 +12,6 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        //Image image = new Image(getClass().getResourceAsStream("about_bg_mitsu.png"));
 
         Scene scene = new Scene(fxmlLoader.load(), 650, 900);
         scene.getStylesheets().add(HelloApplication.class.getResource("mystyle.css").toExternalForm());
